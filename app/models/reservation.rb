5e8646845f295cdbdf6b39eaf8390fe8a6eacad3
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-08-12 19:20:42 +0300

    Add relations and validations for the reservations

---
+++ app/models/reservation.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: reservations
+#
+#  id         :bigint           not null, primary key
+#  end_date   :date
+#  fee        :decimal(, )
+#  start_date :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_reservations_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class Reservation < ApplicationRecord
+  belongs_to :user
+  #belongs_to :destination
+
+  validates :fee, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+end
