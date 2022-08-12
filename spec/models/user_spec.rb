a794c02fa7bcf50eed212eb334a51cfd13ea75d6
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-12 23:14:22 +0300

    Test user model is password is valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -127,5 +127,27 @@ RSpec.describe User, type: :model do
         expect(build(:user, phone_number: '+15555555555')).to be_valid
       end
     end
+
+    context 'when the password is valid' do
+      it 'is invalid when the password is null' do
+        expect(build(:user, password: nil)).to_not be_valid
+      end
+
+      it 'is invalid when the password is empty' do
+        expect(build(:user, password: '')).to_not be_valid
+      end
+
+      it 'is invalid when the password is too long' do
+        expect(build(:user, password: 'a' * 21)).to_not be_valid
+      end
+
+      it 'is invalid when the password is too short' do
+        expect(build(:user, password: 'a')).to_not be_valid
+      end
+
+      it 'is invalid when the password is not confirmed' do
+        expect(build(:user, password_confirmation: 'not the same')).to_not be_valid
+      end
+    end
   end
 end
