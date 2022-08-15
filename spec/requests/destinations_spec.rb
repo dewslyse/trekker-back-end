93d401c2dc8ae1771dbd4dacda90149b591bef1a
buhariaminat <buhariaminat@yahoo.com>
2022-08-15 13:45:34 +0100

    Fix linters

--- spec/requests/destinations_spec.rb
+++ spec/requests/destinations_spec.rb
@@ -12,116 +12,116 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/destinations", type: :request do
+RSpec.describe '/destinations', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Destination. As you add validations to Destination, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    skip('Add a hash of attributes valid for your model')
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    skip('Add a hash of attributes invalid for your model')
+  end
 
   # This should return the minimal set of values that should be in the headers
   # in order to pass any filters (e.g. authentication) defined in
   # DestinationsController, or in your router and rack
   # middleware. Be sure to keep this updated too.
-  let(:valid_headers) {
+  let(:valid_headers) do
     {}
-  }
+  end
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Destination.create! valid_attributes
       get destinations_url, headers: valid_headers, as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       destination = Destination.create! valid_attributes
       get destination_url(destination), as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Destination" do
-        expect {
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Destination' do
+        expect do
           post destinations_url,
                params: { destination: valid_attributes }, headers: valid_headers, as: :json
-        }.to change(Destination, :count).by(1)
+        end.to change(Destination, :count).by(1)
       end
 
-      it "renders a JSON response with the new destination" do
+      it 'renders a JSON response with the new destination' do
         post destinations_url,
              params: { destination: valid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:created)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Destination" do
-        expect {
+    context 'with invalid parameters' do
+      it 'does not create a new Destination' do
+        expect do
           post destinations_url,
                params: { destination: invalid_attributes }, as: :json
-        }.to change(Destination, :count).by(0)
+        end.to change(Destination, :count).by(0)
       end
 
-      it "renders a JSON response with errors for the new destination" do
+      it 'renders a JSON response with errors for the new destination' do
         post destinations_url,
              params: { destination: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        skip('Add a hash of attributes valid for your model')
+      end
 
-      it "updates the requested destination" do
+      it 'updates the requested destination' do
         destination = Destination.create! valid_attributes
         patch destination_url(destination),
               params: { destination: new_attributes }, headers: valid_headers, as: :json
         destination.reload
-        skip("Add assertions for updated state")
+        skip('Add assertions for updated state')
       end
 
-      it "renders a JSON response with the destination" do
+      it 'renders a JSON response with the destination' do
         destination = Destination.create! valid_attributes
         patch destination_url(destination),
               params: { destination: new_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "renders a JSON response with errors for the destination" do
+    context 'with invalid parameters' do
+      it 'renders a JSON response with errors for the destination' do
         destination = Destination.create! valid_attributes
         patch destination_url(destination),
               params: { destination: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested destination" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested destination' do
       destination = Destination.create! valid_attributes
-      expect {
+      expect do
         delete destination_url(destination), headers: valid_headers, as: :json
-      }.to change(Destination, :count).by(-1)
+      end.to change(Destination, :count).by(-1)
     end
   end
 end
