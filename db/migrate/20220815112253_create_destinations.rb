60825b1f4446b78752df081f9e7595e7a1eb65e1
buhariaminat <buhariaminat@yahoo.com>
2022-08-15 13:24:32 +0100

    Update schema

--- db/migrate/20220815112253_create_destinations.rb
+++ db/migrate/20220815112253_create_destinations.rb
@@ -1,6 +1,11 @@
 class CreateDestinations < ActiveRecord::Migration[7.0]
   def change
     create_table :destinations do |t|
+      t.string :name
+      t.string :city_name
+      t.text :description
+      t.string :image_url 
+      t.references :user, null: false, foreign_key: {to_table: :users}
 
       t.timestamps
     end
--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: destinations
+#
+#  id          :bigint           not null, primary key
+#  city_name   :string
+#  description :text
+#  image_url   :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_destinations_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Destination, type: :model do
