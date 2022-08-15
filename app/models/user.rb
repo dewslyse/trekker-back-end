8d2460345e6da5e5cc63fade1c32f28d28fbf268
dewslyse <dewslyse@gmail.com>
2022-08-15 18:02:28 +0000

    Create User controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@
 #  id                     :bigint           not null, primary key
 #  email                  :string           not null
 #  encrypted_password     :string           default(""), not null
-#  first_name             :string           not null
-#  last_name              :string
+#  full_name              :string           not null
 #  phone_number           :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
@@ -26,11 +25,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # has_many :destinations, dependent: :destroy
+  
+  has_many :destinations, dependent: :destroy
   has_many :reservations, dependent: :destroy
+
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
-  validates :first_name, presence: true, length: { minimum: 3, maximum: 20 }
-  validates :last_name, length: { minimum: 3, maximum: 20 }
+  validates :full_name, presence: true, length: { minimum: 3, maximum: 20 }
   PHONE_REGEX_COUNTRY = /\+(?:[0-9] ?){6,14}[0-9]/
   validates :phone_number, length: { minimum: 6, maximum: 14 },
                            format: { with: PHONE_REGEX_COUNTRY, message: 'Please enter a valid number' }
