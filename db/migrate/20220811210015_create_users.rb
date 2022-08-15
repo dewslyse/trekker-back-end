46d2070486c59df851c92401cb3f892392eab041
dewslyse <dewslyse@gmail.com>
2022-08-15 18:01:23 +0000

    Reset migrations

--- db/migrate/20220811210015_create_users.rb
+++ db/migrate/20220811210015_create_users.rb
@@ -2,8 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :email, null: false
-      t.string :first_name, null: false
-      t.string :last_name
+      t.string :full_name, null: false
       t.string :phone_number
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_112253) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_161347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "reservations", force: :cascade do |t|
-    t.bigint "user_id"
-    t.decimal "fee"
-    t.date "start_date"
-    t.date "end_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_reservations_on_user_id"
-  end
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
@@ -35,10 +25,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_112253) do
     t.index ["user_id"], name: "index_destinations_on_user_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "destination_id"
+    t.decimal "fee"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["destination_id"], name: "index_reservations_on_destination_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "first_name", null: false
-    t.string "last_name"
+    t.string "full_name", null: false
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -52,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_112253) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "reservations", "users"
   add_foreign_key "destinations", "users"
+  add_foreign_key "reservations", "destinations"
+  add_foreign_key "reservations", "users"
 end
