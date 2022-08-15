eba35aca45ecb25836a125a9a52d83fdcf6371f6
dewslyse <dewslyse@gmail.com>
2022-08-15 23:39:38 +0000

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  resources :destinations
-  devise_for :users
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
-
+  
   namespace :api do
-    resources :reservations
+    namespace :v1 do
+      resources :destinations
+      resources :reservations
+      post '/login', to: 'sessions#login'
+      post '/register', to: 'sessions#register'
+    end
   end
 end
