030ee505ab9e2666d6ca88bc8951707c217e685c
dewslyse <dewslyse@gmail.com>
2022-08-15 18:10:35 +0000

    Add associations and validations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,30 @@
+# == Schema Information
+#
+# Table name: destinations
+#
+#  id          :bigint           not null, primary key
+#  city_name   :string
+#  description :text
+#  image_url   :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_destinations_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Destination < ApplicationRecord
+  belongs_to :user
+  has_many :reservations, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :city_name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :description, presence: true, length: { minimum: 3, maximum: 500 }
+  validates :image_url, presence: true, length: { minimum: 3, maximum: 50 }
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,25 +2,28 @@
 #
 # Table name: reservations
 #
-#  id         :bigint           not null, primary key
-#  end_date   :date
-#  fee        :decimal(, )
-#  start_date :date
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint
+#  id             :bigint           not null, primary key
+#  end_date       :date
+#  fee            :decimal(, )
+#  start_date     :date
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  destination_id :bigint
+#  user_id        :bigint
 #
 # Indexes
 #
-#  index_reservations_on_user_id  (user_id)
+#  index_reservations_on_destination_id  (destination_id)
+#  index_reservations_on_user_id         (user_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (destination_id => destinations.id)
 #  fk_rails_...  (user_id => users.id)
 #
 class Reservation < ApplicationRecord
   belongs_to :user
-  # belongs_to :destination
+  belongs_to :destination
 
   validates :start_date, presence: true
   validates :end_date, presence: true
