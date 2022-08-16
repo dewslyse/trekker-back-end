2a0e4c0a82d3b227bf8729ab0ec69f859b420982
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-16 14:51:24 +0300

    Test model#reservation valid factory

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -24,5 +24,9 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it 'has a valid factory' do
+      expect(build(:reservation)).to be_valid
+    end
+  end
 end
