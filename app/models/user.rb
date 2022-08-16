7c9d238e09986e5a688b305b9a58d4f453116a24
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-16 14:23:35 +0300

    Validates user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
   validates :full_name, presence: true, length: { minimum: 3, maximum: 20 }
   EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\z/
-  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX, message: 'Please enter a valid email address' }
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: EMAIL_REGEX, message: 'Please enter a valid email address' }
   validates :password, presence: true, length: { minimum: 6, maximum: 20 }, confirmation: true
-  validates :password_confirmation, presence: true, length: { minimum: 6, maximum: 20 }, comparison: { with: :password }
+  validates :password_confirmation, presence: true, length: { minimum: 6, maximum: 20 },
+                                    comparison: { equal_to: :password }
 end
