774e792c7e06ce4a97c6a1e7ec250900699b7740
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-16 15:06:19 +0300

    Test model#destination valid name

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -26,5 +26,23 @@ RSpec.describe Destination, type: :model do
     it 'has a valid factory' do
       expect(build(:destination)).to be_valid
     end
+
+    context 'when the name is valid' do
+      it 'is invalid when the name is null' do
+        expect(build(:destination, name: nil)).to_not be_valid
+      end
+
+      it 'is invalid when the name is empty' do
+        expect(build(:destination, name: '')).to_not be_valid
+      end
+
+      it 'is invalid when the name is less than 3 characters' do
+        expect(build(:destination, name: 'ab')).to_not be_valid
+      end
+
+      it 'is invalid when the name is more than 30 characters' do
+        expect(build(:destination, name: 'a' * 31)).to_not be_valid
+      end
+    end
   end
 end
