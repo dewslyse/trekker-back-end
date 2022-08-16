7ca771d086f4fa2f595238abaea574164b0e6e05
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-16 12:55:48 +0300

    Modify validation for reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :destination
 
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :fee, presence: true
+  validates :start_date, presence: true, comparison: { greater_than_or_equal_to: Date.today }
+  validates :end_date, presence: true, comparison: { greater_than_or_equal_to: :start_date }
+  validates :fee, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
