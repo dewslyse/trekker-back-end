370aaa8c44844794bf811b54bd6516bec4ad2526
dewslyse <dewslyse@gmail.com>
2022-08-17 10:35:42 +0000

    Redirect root path to api-docs path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  # devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  # Redirects root path to api-docs path
+  get '/' => redirect('/api-docs')
   
   namespace :api do
     namespace :v1 do
