ac620facbe45b5e3df2a474213de1ab8693b10bf
dewslyse <dewslyse@gmail.com>
2022-08-18 09:57:10 +0000

    Add destinations to api documentation

--- spec/requests/api/api_spec.rb
+++ spec/requests/api/api_spec.rb
@@ -1,6 +1,7 @@
 require 'swagger_helper'
 
 RSpec.describe 'Trekker API', type: :request do
+  # User Registration
   path '/api/v1/registrations' do
     post 'Register' do
       tags 'Users'
@@ -9,10 +10,10 @@ RSpec.describe 'Trekker API', type: :request do
         type: :object,
         properties: {
           full_name: { type: :string },
-          username: { type: :string, example: 'tester' },
-          email: { type: :string, example: 'tester@example.com' },
-          password: { type: :string, example: 'password' },
-          password_confirmation: { type: :string, example: 'password' }
+          username: { type: :string },
+          email: { type: :string },
+          password: { type: :string },
+          password_confirmation: { type: :string }
         },
         required: %w[full_name username email password password_confirmation]
       }
@@ -32,6 +33,7 @@ RSpec.describe 'Trekker API', type: :request do
     end
   end
 
+  # User Login
   path '/api/v1/sessions' do
     post 'Sign in' do
       tags 'Users'
@@ -54,6 +56,41 @@ RSpec.describe 'Trekker API', type: :request do
 
       response '401', 'Invalid username or password' do
         let(:user) { { username: 'tester' } }
+        run_test!
+      end
+    end
+  end
+
+  # Destinations
+  path '/api/v1/destinations' do
+    get 'Retrieve Destinations' do
+      tags 'Destinations'
+
+      response '200', 'destinations found' do
+        after do |example|
+          example.metadata[:response][:examples] = {
+            'application/json' => JSON.parse(response.body, symbolize_names: true)
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  # Single Destination
+  path '/api/v1/destinations/{id}' do
+    get 'Retrieve Destination' do
+      tags 'Destinations'
+      parameter name: :id, in: :path, type: :integer
+
+      response '200', 'destination found' do
+        let(:id) { create(:destination).id }
+
+        after do |example|
+          example.metadata[:response][:examples] = {
+            'application/json' => JSON.parse(response.body, symbolize_names: true)
+          }
+        end
         run_test!
       end
     end
