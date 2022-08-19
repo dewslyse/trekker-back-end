99c94c3393dbe61ecb72a577c736352d65c90b55
dewslyse <dewslyse@gmail.com>
2022-08-19 20:30:45 +0000

    Update origins url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,16 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001"
-
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
-
-  allow do
-    origins "" #hostsite
+    origins "http://localhost:3000", "http://localhost:3001", "https://mv-trekker-api.herokuapp.com"
 
     resource "*",
       headers: :any,
