9d56073fe5e63b3884fa66e59cd76b3f93f20077
dewslyse <dewslyse@gmail.com>
2022-08-19 15:45:43 +0000

    Add api route to session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "development"
   Rails.application.config.session_store :cookie_store, key: "_rails_app"
 else
-  Rails.application.config.session_store :cookie_store, key: "_rails_app", domain: "example.com"
+  Rails.application.config.session_store :cookie_store, key: "_rails_app", domain: "https://mv-trekker-api.herokuapp.com"
 end
