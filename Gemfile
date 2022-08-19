8e4848fc29fba815d3ce9466e85ceba0b7e7618a
dewslyse <dewslyse@gmail.com>
2022-08-19 16:37:10 +0000

    Add same site gem

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'factory_bot_rails'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
 
+gem 'rails_same_site_cookie'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,1 @@
-if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "mv-trekker-api.herokuapp.com/api/v1"
-else
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
-end
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :none, secure: true
