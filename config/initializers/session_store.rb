c7cf2e33e57f89114dfcc40672e7dba780c6e644
dewslyse <dewslyse@gmail.com>
2022-08-19 20:31:28 +0000

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,1 @@
-if Rails.env == "development"
-  Rails.application.config.session_store :cookie_store, key: "_rails_app"
-else
-  Rails.application.config.session_store :cookie_store, key: "_rails_app", domain: "https://mv-trekker-api.herokuapp.com/"
-end
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", same_site: :none, secure: true
