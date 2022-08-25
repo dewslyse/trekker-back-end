0a368fc4e86b26ba68aa0c7d55737186c662948c
dewslyse <dewslyse@gmail.com>
2022-08-25 09:24:21 +0000

    Fix failing reservations test

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Reservation', type: :request do
   before(:example) do
-    @user = build(:user)
-    @user.save
-    @destination = build(:destination)
-    @destination.save
-    @reservation = build(:reservation)
+    load Rails.root + 'db/seeds.rb'
+    # @user = build(:user)
+    # @user.save
+    # @destination = build(:destination)
+    # @destination.save
+    # @reservation = build(:reservation)
 
-    post '/api/v1/sessions', params: { username: @user.username, password: @user.password }
+    post '/api/v1/sessions', params: { username: 'selase', password: 'password' }
   end
 
   describe 'GET #index' do
     it 'returns http success' do
-      get api_v1_destination_reservations_path(@destination.id)
+      get api_v1_reservations_path
       expect(response).to have_http_status(:success)
     end
   end
@@ -21,7 +22,7 @@ RSpec.describe 'Reservation', type: :request do
   describe 'POST#create' do
     it 'returns http success' do
       post '/api/v1/destinations/:destination_id/reservations', params: {
-        start_date: '2020-01-01', end_date: '2020-01-01', fee: '100', destination_id: @destination.id
+        start_date: '2020-01-01', end_date: '2020-01-05'
       }
 
       expect(response).to have_http_status(:unprocessable_entity)
