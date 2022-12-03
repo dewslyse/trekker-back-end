2f95be27e92f6a735a97539c139eba0c18ab7485
dewslyse <dewslyse@gmail.com>
2022-12-03 21:27:54 +0000

    Require asset gem in production

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,13 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module TrekkerBackEnd
   class Application < Rails::Application
